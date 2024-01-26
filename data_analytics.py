8ba25609d20ca8be8b073b2843bde02b8876c4e0
Justin Koay <justintzeji@gmail.com>
2024-01-26 09:37:27 +0800

    log url

--- data_analytics.py
+++ data_analytics.py
@@ -359,6 +359,7 @@ Most Active donor groups:
 
 def send__telegram_photo(token, chat_id, image_path, image_caption=""):
     url = f"https://api.telegram.org/bot{token}/sendPhoto?chat_id={chat_id}&caption={image_caption}"
+    print(url)
     with open(image_path, "rb") as image_file:
         ret = requests.post(url, files={"photo": image_file})
     print(ret.json())
